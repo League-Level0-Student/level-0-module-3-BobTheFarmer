69ff4c31195d57a0bb64d3abf8771c1686903a87
League Student <league@iMac-26.attlocal.net>
2019-06-12 18:23:21 -0700

    High Low Game IP :thumbsup: :smile: :

--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -5,18 +5,21 @@ package _03_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		Random random = new Random();
+		int yeet = random.nextInt(99) + 1;
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(yeet);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+JOptionPane.showInputDialog(null, "Enter a number or ur a man baby");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
 			
 			// 5. if the guess is correct
