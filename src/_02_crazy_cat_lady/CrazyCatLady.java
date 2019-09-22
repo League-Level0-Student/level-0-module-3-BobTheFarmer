cc7d8c1f81ae32c25e53a2018c90a59a61df3521
League Student <league@iMac-24.attlocal.net>
2019-09-21 17:25:57 -0700

    Finished module thing :smile:
    fi rgseuidg

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,20 +5,33 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String cats = JOptionPane.showInputDialog(null, "How many cats u have?");
 		// 2. Convert their answer into an int
-
+int Cats = Integer.parseInt(cats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(Cats>2) {
+	JOptionPane.showMessageDialog(null, "U a crazy cat lady");
 
-	if(cats<3) {
+}
+	
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+if(Cats<3 && Cats>0) {
+	JOptionPane.showMessageDialog(null, "*Insert cat video here*");
+}
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-	}
+else if(Cats==0){
+
+	JOptionPane.showMessageDialog(null, "*Insert frog on bench vid here*");
+}
+}
+
+	
 
 	static void playVideo(String videoURL) {
 		try {
